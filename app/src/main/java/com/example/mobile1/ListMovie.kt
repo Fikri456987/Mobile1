edbac09a1f5032274b03b32045eb722658024950
Fikri Muhammad Setiyawan <19572002@stmik.jayakarta.ac.id>
2022-12-13 20:43:25 +0700

    Initial commit

---
+++ app/src/main/java/com/example/mobile1/ListMovie.kt
@@ -0,0 +1,50 @@
+package com.example.mobile1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+import com.example.mobile1.`interface`.TmdbEndpoints
+import com.example.mobile1.adapter.MainAdapter
+import com.example.mobile1.models.PopularMovies
+import com.example.mobile1.services.ServiceBuilder
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+
+
+class ListMovie : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_list_movie)
+        val rvUser: RecyclerView = findViewById(R.id.recyclerview)
+        getMovieNew(rvUser)
+    }
+    fun getMovieNew(recyclerView: RecyclerView){
+        val request = ServiceBuilder.buildService(TmdbEndpoints::class.java)
+        val call = request.getMovies(getString(R.string.api_key))
+
+        call.enqueue(object : Callback<PopularMovies>{
+            override fun onResponse(call: Call<PopularMovies>, response: Response<PopularMovies>) {
+                if (response.isSuccessful){
+
+                    recyclerView.apply {
+                        setHasFixedSize(true)
+                        layoutManager = LinearLayoutManager(this@ListMovie)
+                        adapter = MainAdapter(response.body()!!.result)
+                    }
+                    Log.e("resp",response.body().toString())
+                }
+            }
+            override fun onFailure(call: Call<PopularMovies>, t: Throwable) {
+                Toast.makeText(this@ListMovie, "${t.message}", Toast.LENGTH_SHORT).show()
+            }
+        })
+
+    }
+
+}
---
+++ app/src/main/java/interface/TmdbEndpoints.kt
@@ -0,0 +1,14 @@
+package com.example.mobile1.`interface`
+
+import com.example.mobile1.models.PopularMovies
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface TmdbEndpoints {
+
+    @GET("/3/movie/popular")
+    fun getMovies(@Query("api_key") key: String): Call<PopularMovies>
+
+}
+
